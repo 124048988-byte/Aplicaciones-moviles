0ae9a4dcd05f0e9e287e9befb312e9d86e2770e3
Fernando0206 <124048988@upq.edu.mx>
2025-09-10 17:06:27 -0600

    Tipos de datos
    Escribí los tipos de datos que se pueden poner en Java Script

--- jsParaPrincipiantes/app.js
+++ jsParaPrincipiantes/app.js
@@ -1,1 +1,28 @@
-document.write("<h1>Hola, Mundo</h1>");
+//Tipos de datos
+"Hola, Mundo" //String
+'hOLA, Mundo' //String
+
+10000 //number
+-2.3 //number 
+
+
+true  //Boolean
+false //Boolean
+
+['joey', 'Ryan', 'Martha'] //Array
+[1, 2, 3]                  //Array
+[true, false, true, false] //Array
+
+
+// Object
+{
+    "Username": 'Ryan',
+    "Score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
+
+
+
+console.log("Esto es una cadena");
+    

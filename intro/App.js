fa2a2708727b9ea0cad0f1d6dfb8d20e389f72e1
Fernando0206 <124048988@upq.edu.mx>
2025-10-08 20:36:02 -0600

    Contador con botones
    Escribimos ciertos botones como "Agregar", "Quitar" y "Reiniciar" con ciertas propiedadades (title, onPress,etc).  Declaramos cada componente que vamos a usar. Además de recordar que hicimos uso de una función flecha al momento de asignar la propiedad OnPress.

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+//1. imports: Zona de importaciones 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button} from 'react-native'; //Aquí agregamos los componentes que tenemos en este archivo
+import React,{useState} from 'react'; //Esto es un hook
+
+
+
+
+//2. Main: Zona de componentes
 export default function App() {
+
+  const [contador,setContador] = useState(0); //Aquí estamos destructurando un objeto. El 0 lo pongo porque inicialicé el contador en 0. Esto es un componente
+  
+
   return (
     <View style={styles.container}>
-      <Text>Hola, mundo React Native </Text>
+      <Text>Contador: {contador}</Text>
+     {/*  <Button></Button> */} {/* Si vas a declsrar propiedad y agregar algo dentro del botón */}
+      <Button  title="Agregar" onPress={()=>setContador(contador+1)}/>
+      <Button title="Quitar" onPress={()=> setContador(contador-1)}/>
+      <Button title="Reiniciar" onPress={()=>setContador(contador-contador)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
+//3. Estilos: Zona de estetica y posicionamiento 
 const styles = StyleSheet.create({
   container: {
     flex: 1,

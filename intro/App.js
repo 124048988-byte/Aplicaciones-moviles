319bcf5d01ea383b8a29568c5655343624b88e73
Fernando0206 <124048988@upq.edu.mx>
2025-10-09 19:25:47 -0600

    Estilos de mi contador
    Cree una carpeta y un archivo nuevo (screens y ContadorScreen) donde copié el código de App.js y ahí agregué algunos estilos a mis botones y elementos que aparecían en mi programa, tales como "Conteiner", "texto", "texto2", y "botonesConteiner" .

--- intro/App.js
+++ intro/App.js
@@ -1,38 +1,20 @@
-
 //1. imports: Zona de importaciones 
+
+//Esta pantalla servirá para solo mostrar cada "screen" que hagamos hasta el segundo parcial 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native'; //Aquí agregamos los componentes que tenemos en este archivo
-import React,{useState} from 'react'; //Esto es un hook
-
-
+import ContadorScreen from './screens/ContadorScreen'; 
 
 
 //2. Main: Zona de componentes
 export default function App() {
 
-  const [contador,setContador] = useState(0); //Aquí estamos destructurando un objeto. El 0 lo pongo porque inicialicé el contador en 0. Esto es un componente
   
-
   return (
-    <View style={styles.container}>
-      <Text>Contador: {contador}</Text>
-     {/*  <Button></Button> */} {/* Si vas a declsrar propiedad y agregar algo dentro del botón */}
-      <Button  title="Agregar" onPress={()=>setContador(contador+1)}/>
-      <Button title="Quitar" onPress={()=> setContador(contador-1)}/>
-      <Button title="Reiniciar" onPress={()=>setContador(contador-contador)}/>
-      <StatusBar style="auto" />
-    </View>
+
+    <ContadorScreen></ContadorScreen>
+    
   );
 }
 
 
-
-//3. Estilos: Zona de estetica y posicionamiento 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

6fb646a41e266845558cbd3d6fd19dc55f8d6956
Fernando0206 <124048988@upq.edu.mx>
2025-11-18 23:52:38 -0600

    Practica 16. de Navigation
    Se realizó por primera vez la Navigation en clase

---
+++ Navigation_3er_Parcial/StackNavigation/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import 'react-native-gesture-handler'; 
+
+import Home from './screens/home';
+import Profile from './screens/profile';
+import Settings from './screens/settings';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.navigator initialRouteName= "Home">
+
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Profile" component={HProfileome}/>
+          <Stack.Screen name="Settings" component={Settings}/>
+
+        </Stack.navigator>
+    </NavigationContainer>
+
+  );
+}
+
+
+
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
---
+++ Navigation_3er_Parcial/StackNavigation/home.js
@@ -0,0 +1,69 @@
+import { View,Text, Pressable, StyleSheet} from 'react-native';
+
+export default function Home({navigation})
+{
+    return (
+        <View style={StyleSheet.container}>
+            <Title style={StyleSheet.title}>Bienvenido a la pantalla principal!</Title>
+
+            <Pressable style={[StyleSheet.button, StyleSheet.buttonProfile]} onPress={() => navigation.navigate('profile')}>
+                <Text style={StyleSheet.buttonText}>Ir a Perfil</Text>
+            </Pressable>
+
+            <Pressable style={[styles.button , styles.buttonSettings]} onPress={() => navigation.navigate('Settings')}>
+                <Text style={styles.buttonText}>Ir a configuración</Text>
+            </Pressable>
+
+        </View>
+
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container:
+    {
+        flex:1,
+        backgroundColor:'#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20
+    },
+
+    title:
+    {
+        fontSize:22,
+        fontWeight: 'bold',
+        marginBottom: 40,
+        textAlign:'center',
+    },
+
+    button:
+    {
+        paddingVertical: 12,
+        paddingHorizontal:30,
+        borderRadius: 8,
+        marginBottom: 20,
+        width: '80%',
+        alignItems: 'center',
+    },
+
+    buttonProfile:
+    {
+        backgroundColor: '#007BFF',
+    },
+
+    buttonSettings:
+    {
+        backgroundColor: '#FF8800',
+    },
+
+    buttonText:
+    {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: '600'
+    },
+
+
+});
